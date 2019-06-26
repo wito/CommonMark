7ae46cf5c10f312458cbab36b70784dd9b599881
Williham Totland <williham.totland@gmail.com>
2019-06-26 07:33:57 +0000

    Defines interface.

--- Sources/CommonMark/CommonMark.swift
+++ Sources/CommonMark/CommonMark.swift
@@ -1,3 +1,8 @@
-struct CommonMark {
-    var text = "Hello, World!"
+public extension String {
+
+  /// An XHTML representation of the markdown in the string.
+  var renderedMarkdown: String {
+    return self
+  }
+
 }
